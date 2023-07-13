656ca5f4f70e8bf27e712c5b801af556a2895519
sergiocouter <sergiocouter@gmail.com>
2023-07-12 22:47:32 -0300

    Criado o componente de cesta

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/components/telas/Cesta';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hellow World!</Text>
+      <Cesta />
       <StatusBar style="auto" />
     </View>
   );
