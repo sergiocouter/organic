440c5523e8283f78b448057149ddfec54fa9871c
sergiocouter <sergiocouter@gmail.com>
2023-07-13 23:46:31 -0300

    Instalação da font Montserrat e configuração no app.js para utilização da fonte com o carregamento das telas

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/components/telas/Cesta';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold }from  "@expo-google-fonts/montserrat"
 
 export default function App() {
+
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  })
+
+  if(!fonteCarregada){
+    return(
+      <View />
+    )
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
--- src/components/telas/Cesta.js
+++ src/components/telas/Cesta.js
@@ -48,7 +48,7 @@ const estilos = StyleSheet.create({
         fontSize: 26,
         lineHeight: 42,
         color: "#464646",
-        fontWeight: 900,
+        fontFamily: "MontserratBold",
     },
     containerFazenda: {
         flexDirection: "row",
@@ -62,6 +62,7 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26, 
         marginLeft: 12,
+        fontFamily: "MontserratRegular",
     },
     descricao: {
         color: "#A3A3A3",
