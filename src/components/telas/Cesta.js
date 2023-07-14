1fcc380294b37d2afead812a0674e7ef81b71eaa
sergiocouter <sergiocouter@gmail.com>
2023-07-13 23:18:47 -0300

    Adicionei titulo, descrição e preço no componente Cesta e implementei estilização

--- src/components/telas/Cesta.js
+++ src/components/telas/Cesta.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { StyleSheet, Image, Dimensions, Text } from "react-native"
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native"
 
 import topo from "../../../assets/assets/assets/topo.png"
+import logoFazenda from "../../../assets/assets/logo.png"
 
 const width = Dimensions.get("screen").width
 
@@ -9,6 +10,18 @@ export default function Cesta (){
     return <>
         <Image source={topo} style={estilos.topo}/>
         <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+
+        <View style={estilos.containerView}>
+            <Text style={estilos.nomeDaCesta}>Cesta de Verduras</Text>
+
+            <View style={estilos.containerFazenda}>
+                <Image source={logoFazenda} style={estilos.imagemFazenda}/>
+                <Text style={estilos.nomeDaFazenda}>Fazenda Esperança</Text>
+            </View>
+
+            <Text style={estilos.descricao}>Uma cesta com produtos selecionados da fazenda para sua cozinha</Text>
+            <Text style={estilos.preco}>R$ 40,00</Text>
+        </View>
     </>
 }
 
@@ -26,5 +39,41 @@ const estilos = StyleSheet.create({
         color: "white",
         fontWeight: 900,
         padding: 16,
+    },
+    containerView: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    nomeDaCesta: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontWeight: 900,
+    },
+    containerFazenda: {
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+    imagemFazenda:{
+        width: 32,
+        height:32,
+    },
+    nomeDaFazenda: {
+        fontSize: 16,
+        lineHeight: 26, 
+        marginLeft: 12,
+    },
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    preco: {
+        color: "#2A9F85",
+        fontWeight: 900,
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
     }
+
 })
