2a2178600344e228810f1c72a6c67fff5cee1991
sergiocouter <sergiocouter@gmail.com>
2023-07-18 15:04:22 -0300

    Adicionei o FlatList para reenderizar apenas os itens visíveis na tela.

--- src/components/telas/Cesta/index.js
+++ src/components/telas/Cesta/index.js
@@ -1,23 +1,39 @@
 import React from "react"
-import { ScrollView, StyleSheet, View } from "react-native"
+import { FlatList, StyleSheet, View } from "react-native"
 
 import Topo from "./components/Topo"
 import Detalhes from "./components/Detalhes"
-import Itens from "./components/Itens"
+import Item from "./components/Item"
+import Texto from "../../Texto"
 
 export default function Cesta ({ topo, detalhes, itens}){
-    return <ScrollView>
-
-        <Topo {...topo}/>
-
-        <View style={estilos.containerView}>
-            <Detalhes {...detalhes} />
-            <Itens {...itens}/>
-        </View>
-    </ScrollView>
+    return <>
+        <FlatList 
+            data={itens.lista}
+            renderItem={Item}
+            keyExtractor={({nome}) => nome}
+            ListHeaderComponent={() => {
+                return<>
+                    <Topo {...topo}/>
+                    <View style={estilos.containerView}>
+                        <Detalhes {...detalhes} />
+                        <Texto style={estilos.titulo}>{itens.titulo}</Texto>
+                    </View>
+                </>
+            }}
+        /> 
+    </>
 }
 
 const estilos = StyleSheet.create({
+    titulo: {
+        color: "#464646",
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32,
+    },
     containerView: {
         paddingVertical: 8,
         paddingHorizontal: 16,
